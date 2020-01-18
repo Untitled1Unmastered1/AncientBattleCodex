28c2126805968af4879bd88f42e2436c290416de
Untitled1Unmastered1 <gabriel1994.gp@gmail.com>
2020-01-17 23:45:10 -0500

    apex

---
+++ lib/ancient_battle_codex/battle.rb
@@ -0,0 +1,23 @@
+class AncientBattleCodex::Battle 
+
+
+    def self.hey 
+        puts <<-DOC.gsub /^\s*/, ''
+        14. Battle of Platea       | 479 BC  | Persians VS Greeks 
+        13. Battle of Thermopylae  | 480 BC  | Persians VS Greeks 
+        12. Battle of Red Cliffs   | 208 AD  | Southern Warlords Liu Bei & Sun Quan VS Northern Warlord Cao Cao
+        11. Battle of the Hydaspes | 326 BC  | Macedonians VS Hindu Paurava Kingdom
+        10. Battle of Changping    | 262 BC - 260 BC | State of Qin VS State of Zhao 
+        9.  Battle of Chalons      | 451 AD  | Roman Empire VS Gallic Empire
+        8.  Battle of Kadesh       | 1274 BC | New Kingdom of Egypt VS Hittite Empire 
+        7.  The Siege of Syracuse  | 214 BC – 212 BC |  Roman Republic VS Syracuse, Carthage
+        6.  Battle of the Metaurus | 207 BC  | Carthage VS Roman Republic 
+        5.  Third Servile War      | 73 BC –  71 BC  |  Roman Republic VS Escaped Gladiators
+        4.  Battle of Gaugamela    | 331 BC  | Hellenic League VS Achaemenid Empire 
+        3.  Battle of Salamis      | 480 BC  | Roman Republic VS Parthian Empire 
+        2.  Battle of Carrhae      | 53 BC   | Roman Republic VS Parthian Empire 
+        1.  Battle of Gaixia       | 202 BC  | Han VS Western Chu
+
+        DOC
+    end 
+end 
--- lib/environment.rb
+++ lib/environment.rb
@@ -1,4 +1,5 @@
 require_relative  './ancient_battle_codex/version.rb'
+require_relative './ancient_battle_codex/battle.rb'
 require_relative './ancient_battle_codex/cli.rb'
 
 #this file will act as our environment
